c205250a612a9ab63453d37dd315931032c239ba
jcrada <jcrada@gmail.com>
2012-12-05 08:18:46 +1300

    Discrete term halfway...

---
+++ fuzzylite/src/term/Discrete.cpp
@@ -0,0 +1,49 @@
+/*
+ * Discrete.cpp
+ *
+ *  Created on: 5/12/2012
+ *      Author: jcrada
+ */
+
+#include "Discrete.h"
+
+namespace fl {
+
+    Discrete::Discrete(const std::string& name,
+            const std::vector<scalar>& x,
+            const std::vector<scalar>& y)
+            : Term(name), x(x), y(y) {
+    }
+
+    Discrete::~Discrete() {
+    }
+
+    scalar Discrete::minimum() const{
+        return x[0];
+    }
+
+    scalar Discrete::maximum() const{
+        return x[x.size() - 1];
+    }
+
+    scalar Discrete::membership(scalar mu) const{
+        scalar previous = -std::numeric_limits<scalar>::infinity();
+        scalar next = std::numeric_limits<scalar>::infinity();
+        for (std::size_t i = 0 ; i < x.size(); ++i){
+            if (Op::IsEq(x[i], mu)) return y[i];
+            //approximate on the left
+            if (Op::IsLt(x[i], mu) && Op::IsGt(x[i], previous)){
+                previous = x[i];
+            }
+            //get the immediate next one after mu
+            if (Op::IsGt(x[i], mu)){
+                next = x[i];
+                break;
+            }
+        }
+        //TODO:interpolate y[i] from previous and next;
+
+
+    }
+
+} /* namespace fl */
