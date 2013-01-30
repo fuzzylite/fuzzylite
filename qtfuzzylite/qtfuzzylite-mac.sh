034bc166c3c357b14d866531ba68eda067e442d6
jcrada <jcrada@gmail.com>
2013-01-30 20:34:18 +1300

    CMake bundling in MacOSX

--- qtfuzzylite/qtfuzzylite-mac.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-macdeployqt qtfuzzylite.app
-
-#otool -L executable
-
-#install_name_tool -change PATH/TO/LIB/?.dylib @executable_path/../Frameworks/?.dylib
-
-install_name_tool -change /tmp/fuzzylite/fuzzylite/libfuzzylite-dbg.dylib @executable_path/../Frameworks/libfuzzylite-dbg.dylib qtfuzzylite
