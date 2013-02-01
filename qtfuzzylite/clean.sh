571cac4bdd6006944b1fc2b2e761b20b7087a255
jcrada <jcrada@gmail.com>
2013-02-01 19:14:18 +1300

    More modifications on build scripts from Mac

--- qtfuzzylite/clean.sh
+++ qtfuzzylite/clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 make -f Makefile clean
-rm -rf Makefile CMakeFiles tmp cmake_install.cmake CMakeCache.txt
-
+rm -rf Makefile CMakeFiles CMakeScripts Debug qtfuzzylite.build qtfuzzylite.xcodeproj tmp cmake_install.cmake CMakeCache.txt qtfuzzylite.app
+rm *.h *.cxx Info.plist 
