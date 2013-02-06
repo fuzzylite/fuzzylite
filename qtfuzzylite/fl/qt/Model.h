384a5cfa71616bd8ed20f0e1d95a87ce07ae4f80
jcrada <jcrada@gmail.com>
2013-02-07 10:49:52 +1300

    Licenses

--- qtfuzzylite/fl/qt/Model.h
+++ qtfuzzylite/fl/qt/Model.h
@@ -1,3 +1,23 @@
+/**
+    This file is part of qtfuzzylite.
+
+    qtfuzzylite is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    qtfuzzylite is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with qtfuzzylite.  If not, see <http://www.gnu.org/licenses/>.
+
+    Juan Rada-Vilela, 01 February 2013
+    jcrada@fuzzylite.com
+**/
+
 /*
  * Model.h
  *
