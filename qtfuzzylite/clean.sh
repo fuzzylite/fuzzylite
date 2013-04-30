a5873456946bbe0234e3a50213b0733e5f12efbd
jcrada <jcrada@gmail.com>
2013-04-30 20:03:27 +1200

    Final version 2.5c

--- qtfuzzylite/clean.sh
+++ qtfuzzylite/clean.sh
@@ -2,3 +2,4 @@
 make -f Makefile clean
 rm -rf Makefile CMakeFiles CMakeScripts Debug qtfuzzylite.build qtfuzzylite.xcodeproj tmp cmake_install.cmake CMakeCache.txt qtfuzzylite.app
 rm *.h *.cxx Info.plist 
+rm moc*.cpp
