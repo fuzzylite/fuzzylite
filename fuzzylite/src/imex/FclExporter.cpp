1efcec5fe5688532a6f9e7feffe8b33d62edb825
Juan Rada-Vilela <jcrada@fuzzylite.com>
2015-04-13 17:56:13 +1200

    FuzzyLite License

--- fuzzylite/src/imex/FclExporter.cpp
+++ fuzzylite/src/imex/FclExporter.cpp
@@ -1,24 +1,17 @@
 /*
  Author: Juan Rada-Vilela, Ph.D.
- Copyright (C) 2010-2014 FuzzyLite Limited
- All rights reserved
+ Copyright © 2010-2015 FuzzyLite Limited.
+ All rights reserved.
 
- This file is part of fuzzylite.
+ This file is part of fuzzylite®.
 
- fuzzylite is free software: you can redistribute it and/or modify it under
- the terms of the GNU Lesser General Public License as published by the Free
- Software Foundation, either version 3 of the License, or (at your option)
- any later version.
+ fuzzylite® is free software: you can redistribute it and/or modify it under
+ the terms of the FuzzyLite License included with the software.
 
- fuzzylite is distributed in the hope that it will be useful, but WITHOUT
- ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
- for more details.
+ You should have received a copy of the FuzzyLite License along with 
+ fuzzylite®. If not, see <http://www.fuzzylite.com/license/>.
 
- You should have received a copy of the GNU Lesser General Public License
- along with fuzzylite.  If not, see <http://www.gnu.org/licenses/>.
-
- fuzzylite™ is a trademark of FuzzyLite Limited.
+ fuzzylite® is a registered trademark of FuzzyLite Limited.
 
  */
 #include "fl/imex/FclExporter.h"
