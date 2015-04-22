7c8d847eb31cea5372cc77d5c225d9f5c6200bb3
Juan Rada-Vilela <jcrada@fuzzylite.com>
2015-04-22 18:50:39 +1200

    Inlined functions in ConstructionFactory.h

--- fuzzylite/fl/factory/ConstructionFactory.h
+++ fuzzylite/fl/factory/ConstructionFactory.h
@@ -15,8 +15,8 @@
 
  */
 
-#ifndef FL_FACTORY_H
-#define FL_FACTORY_H
+#ifndef FL_CONSTRUCTIONFACTORY_H
+#define FL_CONSTRUCTIONFACTORY_H
 
 #include "fl/fuzzylite.h"
 
@@ -53,5 +53,90 @@ namespace fl {
 
 }
 
-#endif  /* FL_FACTORY_H */
+/**
+ * Template implementation
+ */
+
+
+#include "fl/Exception.h"
+#include "fl/defuzzifier/Defuzzifier.h"
+#include "fl/hedge/Hedge.h"
+#include "fl/norm/SNorm.h"
+#include "fl/norm/TNorm.h"
+#include "fl/term/Function.h"
+#include "fl/term/Term.h"
+
+
+
+namespace fl {
+
+    template <typename T>
+    inline ConstructionFactory<T>::ConstructionFactory(const std::string& name) : _name(name) {
+
+    }
+
+    template <typename T>
+    inline ConstructionFactory<T>::~ConstructionFactory() {
+    }
+
+    template<typename T>
+    inline std::string ConstructionFactory<T>::name() const {
+        return this->_name;
+    }
+
+    template <typename T>
+    inline void ConstructionFactory<T>::registerConstructor(const std::string& key, Constructor constructor) {
+        this->_constructors[key] = constructor;
+    }
+
+    template <typename T>
+    inline void ConstructionFactory<T>::deregisterConstructor(const std::string& key) {
+        typename std::map<std::string, Constructor>::iterator it = this->_constructors.find(key);
+        if (it != this->_constructors.end()) {
+            this->_constructors.erase(it);
+        }
+    }
+
+    template <typename T>
+    inline bool ConstructionFactory<T>::hasConstructor(const std::string& key) const {
+        typename std::map<std::string, Constructor>::const_iterator it = this->_constructors.find(key);
+        return (it != this->_constructors.end());
+    }
+
+    template <typename T>
+    inline typename ConstructionFactory<T>::Constructor ConstructionFactory<T>::getConstructor(const std::string& key) const {
+        typename std::map<std::string, Constructor>::const_iterator it = this->_constructors.find(key);
+        if (it != this->_constructors.end()) {
+            return it->second;
+        }
+        return fl::null;
+    }
+
+    template <typename T>
+    inline T ConstructionFactory<T>::constructObject(const std::string& key) const {
+        typename std::map<std::string, Constructor>::const_iterator it = this->_constructors.find(key);
+        if (it != this->_constructors.end()) {
+            if (it->second) {
+                return it->second();
+            }
+            return fl::null;
+        }
+        std::ostringstream ss;
+        ss << "[factory error] constructor of " + _name + " <" << key << "> not registered";
+        throw fl::Exception(ss.str(), FL_AT);
+    }
+
+    template <typename T>
+    inline std::vector<std::string> ConstructionFactory<T>::available() const {
+        std::vector<std::string> result;
+        typename std::map<std::string, Constructor>::const_iterator it = this->_constructors.begin();
+        while (it != this->_constructors.end()) {
+            result.push_back(it->first);
+            ++it;
+        }
+        return result;
+    }
+}
+
+#endif  /* FL_CONSTRUCTIONFACTORY_H */
 
