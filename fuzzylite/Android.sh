255dbe4be2613140906f6cc99a32960ca459ee77
chenqi <9468305@qq.com>
2015-07-01 16:54:11 +0800

    read source files from FL_SOURCES & add android bash

---
+++ fuzzylite/Android.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+$(NDK_ROOT)/ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./Android.mk APP_ABI=armeabi-v7a APP_PLATFORM=android-15 APP_STL=gnustl_shared
--- fuzzylite/test_android/test_android.bat
+++ fuzzylite/test_android/test_android.bat
@@ -2,18 +2,19 @@
 
 set ADB=adb.exe
 set ARCH=armeabi-v7a
+set DEVICE=/data/local/tmp
 
 echo ndk-build testfuzzylite
 call %NDK_ROOT%/ndk-build NDK_PROJECT_PATH=. APP_BUILD_SCRIPT=./test_android.mk APP_ABI=%ARCH% APP_PLATFORM=android-15 APP_STL=gnustl_shared APP_PIE=true
 
 echo deploy file to device
-call %ADB% push libs\%ARCH%\libfuzzylite.so /data/local/tmp
-call %ADB% push libs\%ARCH%\libgnustl_shared.so /data/local/tmp
-call %ADB% push libs\%ARCH%\testfuzzylite /data/local/tmp
+call %ADB% push libs\%ARCH%\libfuzzylite.so %DEVICE%
+call %ADB% push libs\%ARCH%\libgnustl_shared.so %DEVICE%
+call %ADB% push libs\%ARCH%\testfuzzylite %DEVICE%
 
 echo run test on device
-call %ADB% shell chmod 751 /data/local/tmp/testfuzzylite
-call %ADB% shell LD_LIBRARY_PATH=/data/local/tmp /data/local/tmp/testfuzzylite
+call %ADB% shell chmod 751 %DEVICE%/testfuzzylite
+call %ADB% shell LD_LIBRARY_PATH=%DEVICE% %DEVICE%/testfuzzylite
 
 echo run test done!
 pause
