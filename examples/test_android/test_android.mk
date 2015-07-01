a387a193ae22689143be6a3a747c8f0ef816015c
chenqi <9468305@qq.com>
2015-07-01 17:01:29 +0800

    move test_android to examples

--- examples/test_android/test_android.mk
+++ examples/test_android/test_android.mk
@@ -2,8 +2,8 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := fuzzylite
-LOCAL_SRC_FILES := $(LOCAL_PATH)/../libs/$(TARGET_ARCH_ABI)/libfuzzylite.so
-LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../
+LOCAL_SRC_FILES := $(LOCAL_PATH)/../../fuzzylite/libs/$(TARGET_ARCH_ABI)/libfuzzylite.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../fuzzylite/
 include $(PREBUILT_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
