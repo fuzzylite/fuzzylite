f1e364d5602fe404ef78ea3ae525f539b8d3ea9a
Juan Rada-Vilela <jcrada@fuzzylite.com>
2016-06-24 17:24:34 +1200

    FuzzyLite License

--- fuzzylite/fl/term/SigmoidDifference.h
+++ fuzzylite/fl/term/SigmoidDifference.h
@@ -1,16 +1,16 @@
 /*
- Copyright © 2010-2016 by FuzzyLite Limited.
+ Copyright (C) 2010-2016 by FuzzyLite Limited.
  All rights reserved.
 
- This file is part of fuzzylite®.
+ This file is part of fuzzylite(R).
 
- fuzzylite® is free software: you can redistribute it and/or modify it under
+ fuzzylite is free software: you can redistribute it and/or modify it under
  the terms of the FuzzyLite License included with the software.
 
  You should have received a copy of the FuzzyLite License along with
- fuzzylite®. If not, see <http://www.fuzzylite.com/license/>.
+ fuzzylite. If not, see <http://www.fuzzylite.com/license/>.
 
- fuzzylite® is a registered trademark of FuzzyLite Limited.
+ fuzzylite(R) is a registered trademark of FuzzyLite Limited.
  */
 
 #ifndef FL_SIGMOIDDIFFERENCE_H
