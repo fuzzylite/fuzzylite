5d183dfede697593f7f467df2d157619414c4740
Juan Rada-Vilela <jcrada@fuzzylite.com>
2017-03-20 21:59:34 +1300

    Copyright 2010-2017

--- fuzzylite/fl/defuzzifier/Centroid.h
+++ fuzzylite/fl/defuzzifier/Centroid.h
@@ -1,6 +1,6 @@
 /*
  fuzzylite (R), a fuzzy logic control library in C++.
- Copyright (C) 2010-2016 FuzzyLite Limited. All rights reserved.
+ Copyright (C) 2010-2017 FuzzyLite Limited. All rights reserved.
  Author: Juan Rada-Vilela, Ph.D. <jcrada@fuzzylite.com>
 
  This file is part of fuzzylite.
