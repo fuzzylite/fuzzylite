0556a0a77a29e2c0dcf543d6b15d4ebe4debaf29
Juan Rada-Vilela, PhD <jcrada@fuzzylite.com>
2024-10-10 21:53:36 +1100

    Debug Windows S11

--- test/TestFactory.cpp
+++ test/TestFactory.cpp
@@ -578,37 +578,6 @@ namespace fuzzylite {
             CHECK_THAT(f.evaluate(), Approximates(29.8125));
         }
 
-        SECTION("Deregister all") {
-            FunctionFactoryAssert(new FunctionFactory).deregister_all();
-        }
-
-        //         SECTION("Assign constructor") {
-        //             FunctionFactory only_operators;
-        //             for (auto function : only_operators.availableFunctions())
-        //                 only_operators.deregisterObject(function);
-        //             FunctionFactory ff;
-        //             ff = only_operators;
-        //             CHECK(ff.availableFunctions() == std::vector<std::string>{});
-        //             CHECK_THAT(ff.availableOperators(), Catch::Matchers::UnorderedEquals(operators));
-        //         }
-        //
-        //         SECTION("Copy constructor with operators") {
-        //             FunctionFactory only_operators;
-        //             for (auto function : only_operators.availableFunctions())
-        //                 only_operators.deregisterObject(function);
-        //             FunctionFactory ff(only_operators);
-        //             CHECK(ff.availableFunctions() == std::vector<std::string>{});
-        //             CHECK_THAT(ff.availableOperators(), Catch::Matchers::UnorderedEquals(operators));
-        //         }
-        //         SECTION("Copy constructor with functions") {
-        //             FunctionFactory only_functions;
-        //             for (auto operator_ : only_functions.availableOperators())
-        //                 only_functions.deregisterObject(operator_);
-        //             FunctionFactory ff(only_functions);
-        //             CHECK(ff.availableOperators() == std::vector<std::string>{});
-        //             CHECK_THAT(ff.availableFunctions(), Catch::Matchers::UnorderedEquals(functions));
-        //         }
-        //
         SECTION("Unary Operators") {
             FunctionFactoryAssert(new FunctionFactory)
                 .unary_operation_equals("!", &Op::logicalNot)
@@ -696,6 +665,37 @@ namespace fuzzylite {
                 .binary_operation_equals("neq", &Op::neq)
                 .binary_operation_equals("pow", &std::pow);
         }
+
+        // SECTION("Deregister all") {
+        //     FunctionFactoryAssert(new FunctionFactory).deregister_all();
+        // }
+        SECTION("Assign constructor") {
+            FunctionFactory only_operators;
+            for (auto function : only_operators.availableFunctions())
+                only_operators.deregisterObject(function);
+            FunctionFactory ff;
+            ff = only_operators;
+            CHECK(ff.availableFunctions() == std::vector<std::string>{});
+            CHECK_THAT(ff.availableOperators(), Catch::Matchers::UnorderedEquals(operators));
+        }
+        //
+        //         SECTION("Copy constructor with operators") {
+        //             FunctionFactory only_operators;
+        //             for (auto function : only_operators.availableFunctions())
+        //                 only_operators.deregisterObject(function);
+        //             FunctionFactory ff(only_operators);
+        //             CHECK(ff.availableFunctions() == std::vector<std::string>{});
+        //             CHECK_THAT(ff.availableOperators(), Catch::Matchers::UnorderedEquals(operators));
+        //         }
+        //         SECTION("Copy constructor with functions") {
+        //             FunctionFactory only_functions;
+        //             for (auto operator_ : only_functions.availableOperators())
+        //                 only_functions.deregisterObject(operator_);
+        //             FunctionFactory ff(only_functions);
+        //             CHECK(ff.availableOperators() == std::vector<std::string>{});
+        //             CHECK_THAT(ff.availableFunctions(), Catch::Matchers::UnorderedEquals(functions));
+        //         }
+        //
     }
 
     TEST_CASE("Factory Manager", "[factory]") {
