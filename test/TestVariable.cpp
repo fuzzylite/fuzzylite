a7946633deb1f7c3c330fc1f124c8a7d2b9dbaa4
Juan Rada-Vilela, PhD <jcrada@fuzzylite.com>
2024-11-19 22:02:40 +1100

    tests: refactor test variables

--- test/TestVariable.cpp
+++ test/TestVariable.cpp
@@ -1,33 +1,27 @@
 /*
 fuzzylite (R), a fuzzy logic control library in C++.
+ Copyright (C) 2010-2017 FuzzyLite Limited. All rights reserved.
+ Author: Juan Rada-Vilela, Ph.D. <jcrada@fuzzylite.com>
 
-Copyright (C) 2010-2024 FuzzyLite Limited. All rights reserved.
-Author: Juan Rada-Vilela, PhD <jcrada@fuzzylite.com>.
+ This file is part of fuzzylite.
 
-This file is part of fuzzylite.
+ fuzzylite is free software: you can redistribute it and/or modify it under
+ the terms of the FuzzyLite License included with the software.
 
-fuzzylite is free software: you can redistribute it and/or modify it under
-the terms of the FuzzyLite License included with the software.
+ You should have received a copy of the FuzzyLite License along with
+ fuzzylite. If not, see <http://www.fuzzylite.com/license/>.
 
-You should have received a copy of the FuzzyLite License along with
-fuzzylite. If not, see <https://github.com/fuzzylite/fuzzylite/>.
+ fuzzylite is a registered trademark of FuzzyLite Limited.
+ */
 
-fuzzylite is a registered trademark of FuzzyLite Limited.
-*/
+#include "Headers.h"
 
-#include <random>
-
-#include "../Headers.h"
-
-namespace fuzzylite {
-
-    auto rng = std::default_random_engine();
-
+namespace fuzzylite { namespace test {
     TEST_CASE("variable of Constant terms is sorted", "[variable][variable]") {
         Variable variable("Variable", -10, 10);
-        for (int i = 0; i <= 20; ++i)
+        for (int i = 20; i >= 0; --i)
             variable.addTerm(new Constant(Op::str(i), i - 10));
-        std::shuffle(std::begin(variable.terms()), std::end(variable.terms()), rng);
+
         FL_DBG(variable.toString());
         REQUIRE(variable.numberOfTerms() == 21);
         variable.sort();
@@ -44,9 +38,8 @@ namespace fuzzylite {
 
     TEST_CASE("variable of Triangle terms is sorted", "[variable][variable]") {
         Variable variable("Variable", -30, 30);
-        for (int i = 0; i <= 20; ++i)
+        for (int i = 20; i >= 0; --i)
             variable.addTerm(new Triangle(Op::str(i), i - 1, i, i + 1));
-        std::shuffle(std::begin(variable.terms()), std::end(variable.terms()), rng);
         FL_DBG(variable.toString());
         REQUIRE(variable.numberOfTerms() == 21);
         variable.sort();
@@ -63,4 +56,4 @@ namespace fuzzylite {
         FL_DBG(variable.toString());
     }
 
-}
+}};
